588761b28a24d0f0222e9400b8609e844a860cbb
Cezar Souza <devrazec@gmail.com>
2022-11-13 22:40:05 +0000

    create seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name'              => 'Admin',
+                'email'             => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password'          => bcrypt('password'),
+                'remember_token'    => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name'              => 'User',
+                'email'             => 'user@user.com',
+                'email_verified_at' => now(),
+                'password'          => bcrypt('password'),
+                'remember_token'    => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        User::insert($users);
+    }
+}
